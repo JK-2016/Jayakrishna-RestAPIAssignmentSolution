645e1937790626d41698760365fa1cd70a5b0061
JayaKrishna <awaitingsravya@gmail.com>
2022-07-20 00:17:36 +0530

    Initial Commit

--- src/main/java/com/employeemanagement/security/WebSecurityConfig.java
+++ src/main/java/com/employeemanagement/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.employeemanagement.security;
 import com.employeemanagement.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,44 +41,60 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	    @Override
 //		Authentication Config for Spring Security
+
 	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	        auth.authenticationProvider(authenticationProvider());
+//	        auth.authenticationProvider(authenticationProvider());
 
 
 			PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 			// In-memory authentication, method chaining
 			auth.inMemoryAuthentication()
                 .withUser("admin")
-				.password(encoder.encode("admin"))
+//				.password(encoder.encode("admin"))
+				.password(passwordEncoder().encode("admin"))
 			    .authorities("ADMIN");
-//				.and()
-//				.withUser("user")
+			auth.inMemoryAuthentication()
+				.withUser("user")
+				.password(passwordEncoder().encode("user"))
 //				.password(encoder.encode("user"))
-//				.authorities("USER") ;
+				.authorities("USER") ;
 ////				.withUser("user").password(encoder.encode("user")).roles("USER");
 //					.withUser("admin").password("admin").roles("ADMIN")
 
 	    }
-
+	//		Authorization Config for Spring Security
 	    @Override
-//		Authorization Config for Spring Security
 	    protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests()
-//					.antMatchers("/api/employees/").hasAnyAuthority("ADMIN","USER")
-					.antMatchers("/api/employees/**").permitAll()
-					.antMatchers("/api/employees/add").hasAuthority("ADMIN")
-					.antMatchers("/**").hasAuthority("ADMIN")
-//					.antMatchers("/api/employees/add").hasAuthority("ADMIN")
-//					.anyRequest().authenticated()
-					.and().httpBasic()
-//					csrf needs to be disabled for performing post requests
-					.and().cors() .and().csrf().disable();
+
+
+		 http.authorizeRequests()
+//          Ant matcher should not include context path in this case /api
+//			wrong ant matcher --> .antMatchers("/api/employees/").hasAnyAuthority("ADMIN","USER")
+
+				 .antMatchers("/employees/add").hasAuthority("ADMIN")
+				 .antMatchers(HttpMethod.DELETE,"/employees/*").hasAuthority("ADMIN")
+				 .antMatchers("/roles/*").hasAuthority("ADMIN")
+				 .antMatchers("/users/*").hasAuthority("ADMIN")
+				 .anyRequest().authenticated()
+				 .and() .formLogin()
+				 .and() .httpBasic().and().cors().and().csrf().disable();
+
+//			http.authorizeRequests()
+////					.antMatchers("api/employees/add").hasRole("ADMIN")
+////
+////					.antMatchers("/api/employees/","/api/employees/*").hasAuthority("ADMIN")
+//					.antMatchers("/**").hasAuthority("ADMIN")
+//
+////					.anyRequest().authenticated()
+//					.and().httpBasic()
+////					csrf needs to be disabled for performing post requests
+//					.and().cors() .and().csrf().disable();
 
 	    }
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public PasswordEncoder getPasswordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers(new String[]{"/h2-console/**"});
